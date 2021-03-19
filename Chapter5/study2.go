7d3cfe6288484e0b094a15d3d3d6aa4ec38cfee1
wuxiaohanDUT <2658343834@qq.com>
2021-03-19 14:01:21 +0800

    function2

---
+++ Chapter5/study2.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var a, b = 3, 4
+	fmt.Println(AddAndSub(a, b))
+	fmt.Println(SubAndAdd(a, b))
+	var c, d = AddAndSub(a, b)
+	var e, _ = SubAndAdd(a, b) //可以忽略其中某个返回值
+	fmt.Println(c, d, e)
+
+}
+
+//多返回值
+func AddAndSub(a, b int) (int, int) {
+	return a + b, a - b
+}
+
+//裸返回
+func SubAndAdd(a, b int) (c, d int) {
+	c = a - b
+	d = a + b
+	return
+}
