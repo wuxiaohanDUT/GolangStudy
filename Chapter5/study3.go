b7a3c93ca2065e5b6a4883d155660bea6ac87448
wuxiaohanDUT <2658343834@qq.com>
2021-03-20 12:56:39 +0800

    param func

---
+++ Chapter5/study3.go
@@ -0,0 +1,33 @@
+package main
+
+/**
+函数变量
+*/
+import (
+	"fmt"
+)
+
+func main() {
+	arr := []int{5, 6, 7, 1, -2, 3}
+	sort(arr, compare1)
+	fmt.Println(arr)
+	sort(arr, compare2)
+	fmt.Println(arr)
+}
+func compare1(a, b int) bool {
+	return a-b > 0
+}
+func compare2(a, b int) bool {
+	return b-a > 0
+}
+
+//函数参数化
+func sort(arr []int, compare func(a, b int) bool) {
+	for i := 0; i < len(arr); i++ {
+		for j := 0; j < len(arr)-i-1; j++ {
+			if compare(arr[j], arr[j+1]) {
+				arr[j], arr[j+1] = arr[j+1], arr[j]
+			}
+		}
+	}
+}
